655b9245d8647865fe847db53912e4b5876a60fa
endy mimi <ighobuendy@gmail.com>
2025-03-27 06:06:01 -0700

    fixes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
-
-
-
+import { signUp } from "../controllers/authController.js"
 
 const router = express.Router();
+
+// sign up route
 
 router.post("/sign-up", signUp);
 
 export default router
+
